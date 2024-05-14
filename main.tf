bb5df67d7df439c0113d461e88105102bc8c1110
BryanFauble <17128019+BryanFauble@users.noreply.github.com>
2024-05-14 12:46:49 -0700

    Revert "Set auth mode to API"
    This reverts commit 4096f3d787f6da5627ce0b7e4afaf314a7a1ea2e.

--- main.tf
+++ main.tf
@@ -128,7 +128,6 @@ module "eks" {
   # Cluster access entry
   # To add the current caller identity as an administrator
   enable_cluster_creator_admin_permissions = true
-  authentication_mode                      = "API"
 
   access_entries = {
     # One access entry with a policy associated
