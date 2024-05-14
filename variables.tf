33e88092731e2e9e6b4abc58ec8c239b3ad7c6e4
BryanFauble <17128019+BryanFauble@users.noreply.github.com>
2024-05-14 10:24:07 -0700

    Remove AWS keys

--- variables.tf
+++ variables.tf
@@ -36,23 +36,6 @@ variable "region" {
   type        = string
   default     = "us-east-1"
 }
-variable "aws_access_key" {
-  description = "AWS access key"
-  type        = string
-  sensitive   = true
-}
-
-variable "aws_secret_key" {
-  description = "AWS secret key"
-  type        = string
-  sensitive   = true
-}
-
-variable "aws_session_token" {
-  description = "AWS session token"
-  type        = string
-  sensitive   = true
-}
 
 variable "spotinst_account" {
   description = "Spot.io account"
