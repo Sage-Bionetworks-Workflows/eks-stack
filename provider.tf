a28135511a21228eb8c1617cfde893742feec963
BryanFauble <17128019+BryanFauble@users.noreply.github.com>
2024-05-14 10:10:48 -0700

    Remove tokens from provider. It'll come from cloud integration

--- provider.tf
+++ provider.tf
@@ -1,8 +1,5 @@
 provider "aws" {
-  region     = var.region
-  access_key = var.aws_access_key
-  secret_key = var.aws_secret_key
-  token      = var.aws_session_token
+  region = var.region
 }
 
 provider "spotinst" {
