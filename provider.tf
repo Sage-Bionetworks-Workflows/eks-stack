4c9046f007fbc4eb4ad5cdb564fcb823b1e7d365
BryanFauble <17128019+BryanFauble@users.noreply.github.com>
2024-05-16 16:49:08 -0700

    Revert "Revert "Correcting k8s provider with kube config path""
    This reverts commit c8658d844213a7857682eb65098b95a8034c09b4.

--- provider.tf
+++ provider.tf
@@ -8,6 +8,7 @@ provider "spotinst" {
 }
 
 provider "kubernetes" {
+  config_path            = "~/.kube/config"
   host                   = data.aws_eks_cluster.cluster.endpoint
   cluster_ca_certificate = base64decode(data.aws_eks_cluster.cluster.certificate_authority[0].data)
   token                  = data.aws_eks_cluster_auth.cluster.token
