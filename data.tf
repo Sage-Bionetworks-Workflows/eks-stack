62a574c4022c7811385c4c35ada5811f4ef46358
BryanFauble <17128019+BryanFauble@users.noreply.github.com>
2024-05-16 15:51:53 -0700

    Deploy VPC through TF

--- data.tf
+++ data.tf
@@ -1,18 +1,18 @@
 data "aws_eks_cluster" "cluster" {
   depends_on = [module.eks.cluster_id]
   # count = var.create_eks ? 1 : 0
-  name       = module.eks.cluster_name
+  name = module.eks.cluster_name
 }
 
 data "aws_eks_cluster_auth" "cluster" {
   depends_on = [module.eks.cluster_id]
   # count = var.create_eks ? 1 : 0
-  name       = module.eks.cluster_name
+  name = module.eks.cluster_name
 }
 
 data "aws_iam_instance_profiles" "profile" {
   depends_on = [module.eks]
-  role_name = module.eks.eks_managed_node_groups["one"].iam_role_name
+  role_name  = module.eks.eks_managed_node_groups["one"].iam_role_name
 }
 # data "aws_iam_instance_profiles" "profile2" {
 #   depends_on = [module.eks]
@@ -20,9 +20,9 @@ data "aws_iam_instance_profiles" "profile" {
 # }
 
 data "aws_secretsmanager_secret" "spotinst_token" {
- name = "spotinst_token"
+  name = "spotinst_token"
 }
 
 data "aws_secretsmanager_secret_version" "secret_credentials" {
- secret_id = data.aws_secretsmanager_secret.spotinst_token.id
+  secret_id = data.aws_secretsmanager_secret.spotinst_token.id
 }
