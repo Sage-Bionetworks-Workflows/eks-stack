99a55d54e7f744274c24aeb424026241eef25145
BryanFauble <17128019+BryanFauble@users.noreply.github.com>
2024-05-16 16:49:43 -0700

    Revert "Revert "Wait to apply k8s controller""
    This reverts commit e38cf7279a9a1341629e7b2ca711ecbccdb56d19.

--- main.tf
+++ main.tf
@@ -225,15 +225,15 @@ module "ocean-aws-k8s" {
 #   # config_map_name = module.eks_auth
 # }
 
-module "kubernetes-controller" {
-  source     = "spotinst/kubernetes-controller/ocean"
-  version    = "0.0.2"
-  depends_on = [module.ocean-aws-k8s]
+# module "kubernetes-controller" {
+#   source     = "spotinst/kubernetes-controller/ocean"
+#   version    = "0.0.2"
+#   depends_on = [module.ocean-aws-k8s]
 
-  # Credentials
-  spotinst_token   = data.aws_secretsmanager_secret_version.secret_credentials.secret_string
-  spotinst_account = var.spotinst_account
+#   # Credentials
+#   spotinst_token   = data.aws_secretsmanager_secret_version.secret_credentials.secret_string
+#   spotinst_account = var.spotinst_account
 
-  # Configuration
-  cluster_identifier = var.cluster_name
-}
+#   # Configuration
+#   cluster_identifier = var.cluster_name
+# }
