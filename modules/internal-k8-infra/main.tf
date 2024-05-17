c162f7876f737edb7e613beace424b010e585e5a
BryanFauble <17128019+BryanFauble@users.noreply.github.com>
2024-05-16 23:06:46 -0700

    Update k8s internal

--- modules/internal-k8-infra/main.tf
+++ modules/internal-k8-infra/main.tf
@@ -1,7 +1,6 @@
 module "kubernetes-controller" {
-  source     = "spotinst/kubernetes-controller/ocean"
-  version    = "0.0.2"
-  depends_on = [module.ocean-aws-k8s]
+  source  = "spotinst/kubernetes-controller/ocean"
+  version = "0.0.2"
 
   # Credentials
   spotinst_token   = data.aws_secretsmanager_secret_version.secret_credentials.secret_string
@@ -11,20 +10,3 @@ module "kubernetes-controller" {
   cluster_identifier = var.cluster_name
 }
 
-module "ocean-aws-k8s" {
-  source  = "spotinst/ocean-aws-k8s/spotinst"
-  version = "1.2.0"
-
-  # Configuration
-  cluster_name                     = var.cluster_name
-  region                           = var.region
-  subnet_ids                       = data.aws_subnet.private.*.id
-  worker_instance_profile_arn      = tolist(data.aws_iam_instance_profiles.profile.arns)[0]
-  security_groups                  = data.aws_security_group.node.id
-  is_aggressive_scale_down_enabled = true
-  max_scale_down_percentage        = 33
-  # Overwrite Name Tag and add additional
-  # tags = {
-  #   "kubernetes.io/cluster/tyu-spot-ocean" = "owned"
-  # }
-}
