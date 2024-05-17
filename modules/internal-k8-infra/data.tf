7d68f866bc45499e2817224171c014cc1aa55f91
BryanFauble <17128019+BryanFauble@users.noreply.github.com>
2024-05-16 23:11:15 -0700

    Clean up internal k8s

--- modules/internal-k8-infra/data.tf
+++ modules/internal-k8-infra/data.tf
@@ -6,36 +6,6 @@ data "aws_eks_cluster_auth" "cluster" {
   name = "dpe-k8"
 }
 
-data "aws_security_group" "node" {
-  id = tolist(data.aws_eks_cluster.cluster.vpc_config[0].cluster_security_group_id)[0]
-}
-
-data "aws_iam_instance_profiles" "profile" {
-  depends_on = [module.eks]
-  role_name  = module.eks.eks_managed_node_groups["one"].iam_role_name
-}
-
-data "aws_vpc" "selected" {
-  filter {
-    name   = "tag:Name"
-    values = ["spacelift-created-vpc"]
-  }
-}
-
-data "aws_subnet" "private" {
-  vpc_id = data.aws_vpc.selected.id
-
-  filter {
-    name   = "tag:Name"
-    values = ["*private*"]
-  }
-}
-
-# data "aws_iam_instance_profiles" "profile2" {
-#   depends_on = [module.eks]
-#   role_name = module.eks.eks_managed_node_groups["two"].iam_role_name
-# }
-
 data "aws_secretsmanager_secret" "spotinst_token" {
   name = "spotinst_token"
 }
