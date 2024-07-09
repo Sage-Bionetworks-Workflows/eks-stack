79d6faaf2212a42ca1f82ca9f3c1faf709ab2724
BryanFauble <17128019+BryanFauble@users.noreply.github.com>
2024-07-09 16:08:21 -0700

    Update space id

--- modules/main.tf
+++ modules/main.tf
@@ -50,6 +50,7 @@ resource "spacelift_module" "sage-aws-eks-autoscaler" {
 resource "spacelift_context" "k8s-kubeconfig" {
   description = "Hooks used to set up the kubeconfig for connecting to the K8s cluster"
   name        = "Kubernetes Deployments Kubeconfig"
+  space_id    = "root"
 
   before_init = [
     "aws eks update-kubeconfig --region $REGION --name $CLUSTER_NAME"
