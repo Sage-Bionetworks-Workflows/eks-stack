303e144d5d12d2ebe1a6f1aa37eec417b7205485
BryanFauble <17128019+BryanFauble@users.noreply.github.com>
2024-07-09 16:28:37 -0700

    Try to set version with spacelift_version resource

--- modules/main.tf
+++ modules/main.tf
@@ -14,6 +14,11 @@ resource "spacelift_module" "sage-aws-vpc" {
   space_id           = "root"
 }
 
+resource "spacelift_version" "sage-aws-vpc-version" {
+  module_id      = spacelift_module.sage-aws-vpc.id
+  version_number = "0.2.0"
+}
+
 resource "spacelift_module" "sage-aws-eks" {
   github_enterprise {
     namespace = "Sage-Bionetworks-Workflows"
@@ -30,6 +35,11 @@ resource "spacelift_module" "sage-aws-eks" {
   space_id           = "root"
 }
 
+resource "spacelift_version" "sage-aws-eks-version" {
+  module_id      = spacelift_module.sage-aws-eks.id
+  version_number = "0.2.0"
+}
+
 resource "spacelift_module" "sage-aws-eks-autoscaler" {
   github_enterprise {
     namespace = "Sage-Bionetworks-Workflows"
@@ -44,4 +54,9 @@ resource "spacelift_module" "sage-aws-eks-autoscaler" {
   repository         = "eks-stack"
   project_root       = "modules/sage-aws-k8s-node-autoscaler"
   space_id           = "root"
+}
+
+resource "spacelift_version" "sage-aws-eks-autoscaler-version" {
+  module_id      = spacelift_module.sage-aws-eks-autoscaler.id
+  version_number = "0.2.0"
 }
