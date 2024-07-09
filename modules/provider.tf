1df915c1d4b8b038eee15409dc5c156dfa644560
BryanFauble <17128019+BryanFauble@users.noreply.github.com>
2024-07-09 12:37:35 -0700

    Set versions tf on module

--- modules/provider.tf
+++
@@ -1,6 +0,0 @@
-provider "spacelift" {
-  # Running from within spacelift does not require these to be set
-  # api_key_endpoint = "https://sagebionetworks.app.spacelift.io"
-  # api_key_id     = ""
-  # api_key_secret = ""
-}
