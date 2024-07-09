c9bf0d1efb031417b07ab23787902aa95c4c6df3
BryanFauble <17128019+BryanFauble@users.noreply.github.com>
2024-07-09 16:29:36 -0700

    Increment module versions

--- environments/dev/dpe-sandbox-k8s-deployments/main.tf
+++ environments/dev/dpe-sandbox-k8s-deployments/main.tf
@@ -1,6 +1,6 @@
 module "sage-aws-eks-autoscaler" {
   source  = "spacelift.io/sagebionetworks/sage-aws-eks-autoscaler/aws"
-  version = "0.1.0"
+  version = "0.2.0"
 
   cluster_name           = "dpe-k8-sandbox"
   private_vpc_subnet_ids = var.private_subnet_ids
--- environments/dev/dpe-sandbox-k8s/main.tf
+++ environments/dev/dpe-sandbox-k8s/main.tf
@@ -1,12 +1,12 @@
 module "sage-aws-vpc" {
   source   = "spacelift.io/sagebionetworks/sage-aws-vpc/aws"
-  version  = "0.1.0"
+  version  = "0.2.0"
   vpc_name = "dpe-sandbox"
 }
 
 module "sage-aws-eks" {
   source  = "spacelift.io/sagebionetworks/sage-aws-eks/aws"
-  version = "0.1.0"
+  version = "0.2.0"
 
   cluster_name           = "dpe-k8-sandbox"
   private_vpc_subnet_ids = module.sage-aws-vpc.private_subnet_ids
