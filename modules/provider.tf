a3ffe84878b944ccf1fa411b70014c42be38b23a
BryanFauble <17128019+BryanFauble@users.noreply.github.com>
2024-07-09 12:30:18 -0700

    Add provider to modules

---
+++ modules/provider.tf
@@ -0,0 +1,6 @@
+provider "spacelift" {
+  # Running from within spacelift does not require these to be set
+  # api_key_endpoint = "https://sagebionetworks.app.spacelift.io"
+  # api_key_id     = ""
+  # api_key_secret = ""
+}
