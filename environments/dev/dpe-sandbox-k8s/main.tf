c26d084b445f6f853f0575a3360ba701835b6945
BryanFauble <17128019+BryanFauble@users.noreply.github.com>
2024-07-09 14:07:51 -0700

    Pass along vpc_id to eks cluster

--- environments/dev/dpe-sandbox-k8s/main.tf
+++ environments/dev/dpe-sandbox-k8s/main.tf
@@ -8,10 +8,11 @@ module "sage-aws-eks" {
   source  = "spacelift.io/sagebionetworks/sage-aws-eks/aws"
   version = "0.1.0"
 
-  vpc_name               = "dpe-sandbox"
   cluster_name           = "dpe-k8-sandbox"
   private_vpc_subnet_ids = module.sage-aws-vpc.private_subnet_ids
+  vpc_id                 = module.sage-aws-vpc.vpc_id
   vpc_security_group_id  = module.sage-aws-vpc.security_group_id
+
 }
 
 module "sage-aws-eks-autoscaler" {
