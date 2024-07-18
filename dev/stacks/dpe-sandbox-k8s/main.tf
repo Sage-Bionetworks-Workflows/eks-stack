90f38efff47dca35fda6185c67e4e05a44f25ff6
BryanFauble <17128019+BryanFauble@users.noreply.github.com>
2024-07-18 15:19:16 -0700

    Updating modules

--- dev/stacks/dpe-sandbox-k8s/main.tf
+++ dev/stacks/dpe-sandbox-k8s/main.tf
@@ -1,12 +1,12 @@
 module "sage-aws-vpc" {
   source   = "spacelift.io/sagebionetworks/sage-aws-vpc/aws"
-  version  = "0.2.2"
+  version  = "0.3.0"
   vpc_name = "dpe-sandbox"
 }
 
 module "sage-aws-eks" {
   source  = "spacelift.io/sagebionetworks/sage-aws-eks/aws"
-  version = "0.2.6"
+  version = "0.3.0"
 
   cluster_name             = "dpe-k8-sandbox"
   private_vpc_subnet_ids   = module.sage-aws-vpc.private_subnet_ids
