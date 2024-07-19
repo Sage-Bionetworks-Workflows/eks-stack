2d6694de86ec9d0db42edf14a02948f15c065c56
BryanFauble <17128019+BryanFauble@users.noreply.github.com>
2024-07-19 15:15:40 -0700

    Leave enforcement on standard

--- dev/stacks/dpe-sandbox-k8s/main.tf
+++ dev/stacks/dpe-sandbox-k8s/main.tf
@@ -8,13 +8,14 @@ module "sage-aws-vpc" {
 
 module "sage-aws-eks" {
   source  = "spacelift.io/sagebionetworks/sage-aws-eks/aws"
-  version = "0.3.1"
+  version = "0.3.2"
 
-  cluster_name             = "dpe-k8-sandbox"
-  private_vpc_subnet_ids   = module.sage-aws-vpc.private_subnet_ids
-  vpc_id                   = module.sage-aws-vpc.vpc_id
-  vpc_security_group_id    = module.sage-aws-vpc.vpc_security_group_id
-  enable_policy_event_logs = true
-  capture_cloudwatch_logs  = true
-  cloudwatch_retention     = 1
+  cluster_name                      = "dpe-k8-sandbox"
+  private_vpc_subnet_ids            = module.sage-aws-vpc.private_subnet_ids
+  vpc_id                            = module.sage-aws-vpc.vpc_id
+  vpc_security_group_id             = module.sage-aws-vpc.vpc_security_group_id
+  enable_policy_event_logs          = true
+  capture_cloudwatch_logs           = true
+  cloudwatch_retention              = 1
+  pod_security_group_enforcing_mode = "standard"
 }
