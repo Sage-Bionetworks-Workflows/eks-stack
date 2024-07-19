00df837ec058684c72b62477b25312918509f8fa
BryanFauble <17128019+BryanFauble@users.noreply.github.com>
2024-07-19 14:14:58 -0700

    Update to use new vpc module

--- dev/stacks/dpe-sandbox-k8s/main.tf
+++ dev/stacks/dpe-sandbox-k8s/main.tf
@@ -1,6 +1,6 @@
 module "sage-aws-vpc" {
   source             = "spacelift.io/sagebionetworks/sage-aws-vpc/aws"
-  version            = "0.3.1"
+  version            = "0.3.2"
   vpc_name           = "dpe-sandbox"
   capture_flow_logs  = true
   flow_log_retention = 1
