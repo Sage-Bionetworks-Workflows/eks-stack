9f7e20680e47d13f15d2b1d63be5bb06d252a161
BryanFauble <17128019+BryanFauble@users.noreply.github.com>
2024-07-19 14:09:16 -0700

    Set cloudwatch retention to 1

--- dev/stacks/dpe-sandbox-k8s/main.tf
+++ dev/stacks/dpe-sandbox-k8s/main.tf
@@ -8,11 +8,13 @@ module "sage-aws-vpc" {
 
 module "sage-aws-eks" {
   source  = "spacelift.io/sagebionetworks/sage-aws-eks/aws"
-  version = "0.3.0"
+  version = "0.3.1"
 
   cluster_name             = "dpe-k8-sandbox"
   private_vpc_subnet_ids   = module.sage-aws-vpc.private_subnet_ids
   vpc_id                   = module.sage-aws-vpc.vpc_id
   vpc_security_group_id    = module.sage-aws-vpc.vpc_security_group_id
   enable_policy_event_logs = true
+  capture_cloudwatch_logs  = true
+  cloudwatch_retention     = 1
 }
