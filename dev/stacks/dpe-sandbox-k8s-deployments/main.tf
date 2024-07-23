106f75a4e3d955935754deda2816cdfa9a22617d
BryanFauble <17128019+BryanFauble@users.noreply.github.com>
2024-07-23 12:25:46 -0700

    increment autoscaler

--- dev/stacks/dpe-sandbox-k8s-deployments/main.tf
+++ dev/stacks/dpe-sandbox-k8s-deployments/main.tf
@@ -1,6 +1,6 @@
 module "sage-aws-eks-autoscaler" {
   source  = "spacelift.io/sagebionetworks/sage-aws-eks-autoscaler/aws"
-  version = "0.3.0"
+  version = "0.3.2"
 
   cluster_name           = var.cluster_name
   private_vpc_subnet_ids = var.private_subnet_ids
