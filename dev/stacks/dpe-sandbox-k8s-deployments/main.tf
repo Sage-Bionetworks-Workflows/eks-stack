af78ec5c338c54e4b296b5a7661b0eb5b0551a28
BryanFauble <17128019+BryanFauble@users.noreply.github.com>
2024-07-23 10:31:30 -0700

    Allow inbound kubelet port from nodes

--- dev/stacks/dpe-sandbox-k8s-deployments/main.tf
+++ dev/stacks/dpe-sandbox-k8s-deployments/main.tf
@@ -18,49 +18,6 @@ module "sage-aws-eks-autoscaler" {
 
 
 # Anything beyond this is used for testing
-# resource "aws_security_group" "allow_tls" {
-#   name        = "allow_tls"
-#   description = "Allow TLS inbound traffic and all outbound traffic"
-#   vpc_id      = aws_vpc.main.id
-
-#   tags = {
-#     Name = "allow_tls"
-#   }
-# }
-
-# resource "aws_vpc_security_group_ingress_rule" "allow_tls_ipv4" {
-#   security_group_id = aws_security_group.allow_tls.id
-#   cidr_ipv4         = var.vpc_cidr_block
-#   from_port         = 443
-#   ip_protocol       = "tcp"
-#   to_port           = 443
-# }
-
-# resource "aws_vpc_security_group_ingress_rule" "allow_tls_ipv6" {
-#   security_group_id = aws_security_group.allow_tls.id
-#   cidr_ipv6         = aws_vpc.main.ipv6_cidr_block
-#   from_port         = 443
-#   ip_protocol       = "tcp"
-#   to_port           = 443
-# }
-
-# resource "aws_vpc_security_group_egress_rule" "allow_all_traffic_ipv4" {
-#   security_group_id = aws_security_group.allow_tls.id
-#   cidr_ipv4         = "0.0.0.0/0"
-#   ip_protocol       = "-1" # semantically equivalent to all ports
-# }
-
-# resource "aws_vpc_security_group_egress_rule" "allow_all_traffic_ipv6" {
-#   security_group_id = aws_security_group.allow_tls.id
-#   cidr_ipv6         = "::/0"
-#   ip_protocol       = "-1" # semantically equivalent to all ports
-# }
-
-resource "kubernetes_namespace" "testing" {
-  metadata {
-    name = "testing-namespace"
-  }
-}
 
 # resource "kubernetes_manifest" "security_group_policy" {
 #   manifest = <<EOF
@@ -150,6 +107,14 @@ resource "aws_security_group" "sg-stars-demo" {
     description = "Allow all traffic to self"
   }
 
+  ingress {
+    from_port       = 10250
+    to_port         = 10250
+    protocol        = "tcp"
+    security_groups = [var.node_security_group_id]
+    description     = "Allow all traffic to self"
+  }
+
   # ingress {
   #   # TODO: If I had any probes like liveness or health checks I would need to explicity allow it here
   #   from_port       = 9001
@@ -230,7 +195,6 @@ resource "kubernetes_manifest" "security_group_policy" {
         groupIds = [
           aws_security_group.sg-stars-demo.id,
           var.pod_to_node_dns_sg_id,
-          "sg-0fe24d1b5771d961f",
         ]
       }
     }
