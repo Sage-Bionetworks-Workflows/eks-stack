f34dca647fd202ef674aa6f61969f513debd97ee
BryanFauble <17128019+BryanFauble@users.noreply.github.com>
2024-07-26 14:29:35 -0700

    Bump capacity for testing

--- dev/stacks/dpe-sandbox-k8s-deployments/main.tf
+++ dev/stacks/dpe-sandbox-k8s-deployments/main.tf
@@ -1,12 +1,13 @@
 module "sage-aws-eks-autoscaler" {
   source  = "spacelift.io/sagebionetworks/sage-aws-eks-autoscaler/aws"
-  version = "0.3.2"
+  version = "0.3.4"
 
   cluster_name           = var.cluster_name
   private_vpc_subnet_ids = var.private_subnet_ids
   vpc_id                 = var.vpc_id
   node_security_group_id = var.node_security_group_id
   spotinst_account       = var.spotinst_account
+  desired_capacity       = 2
 }
 
 module "victoria-metrics" {
