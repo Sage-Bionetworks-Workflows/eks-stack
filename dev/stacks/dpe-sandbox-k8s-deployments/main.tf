89b641f54e3844ca685b62c9fd2af32f33a3c3c8
BryanFauble <17128019+BryanFauble@users.noreply.github.com>
2024-07-31 12:23:03 -0700

    Deploy airflow

--- dev/stacks/dpe-sandbox-k8s-deployments/main.tf
+++ dev/stacks/dpe-sandbox-k8s-deployments/main.tf
@@ -19,3 +19,9 @@ module "trivy-operator" {
   source  = "spacelift.io/sagebionetworks/trivy-operator/aws"
   version = "0.0.12"
 }
+
+module "airflow" {
+  source       = "spacelift.io/sagebionetworks/airflow/aws"
+  version      = "0.0.1"
+  cluster_name = var.cluster_name
+}
