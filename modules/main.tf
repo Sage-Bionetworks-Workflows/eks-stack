b2c577c9942bde61e590008e4f55ce5a4936b2e2
BryanFauble <17128019+BryanFauble@users.noreply.github.com>
2024-07-31 13:57:51 -0700

    Update eks module

--- modules/main.tf
+++ modules/main.tf
@@ -1,5 +1,57 @@
 locals {
   spacelift_modules = {
+
+    eks = {
+      github_enterprise = {
+        namespace = "Sage-Bionetworks-Workflows"
+        id        = "sage-bionetworks-workflows-gh"
+      }
+      repository = "eks-stack"
+
+      name               = "sage-aws-eks"
+      terraform_provider = "aws"
+      administrative     = false
+      branch             = var.branch
+      description        = "Terraform module for creating an EKS cluster in AWS"
+      project_root       = "modules/sage-aws-eks"
+      space_id           = "root"
+      version_number     = "0.4.0"
+    }
+
+    vpc = {
+      github_enterprise = {
+        namespace = "Sage-Bionetworks-Workflows"
+        id        = "sage-bionetworks-workflows-gh"
+      }
+      repository = "eks-stack"
+
+      name               = "sage-aws-vpc"
+      terraform_provider = "aws"
+      administrative     = false
+      branch             = var.branch
+      description        = "Terraform module for creating a VPC in AWS"
+      project_root       = "modules/sage-aws-vpc"
+      space_id           = "root"
+      version_number     = "0.3.4"
+    }
+
+    eks-autoscaler = {
+      github_enterprise = {
+        namespace = "Sage-Bionetworks-Workflows"
+        id        = "sage-bionetworks-workflows-gh"
+      }
+      repository = "eks-stack"
+
+      name               = "sage-aws-eks-autoscaler"
+      terraform_provider = "aws"
+      administrative     = false
+      branch             = var.branch
+      description        = "Terraform module for creating an EKS cluster autoscaler in AWS"
+      project_root       = "modules/sage-aws-k8s-node-autoscaler"
+      space_id           = "root"
+      version_number     = "0.3.4"
+    }
+
     victoria-metrics = {
       github_enterprise = {
         namespace = "Sage-Bionetworks-Workflows"
@@ -10,7 +62,7 @@ locals {
       name               = "victoria-metrics"
       terraform_provider = "aws"
       administrative     = false
-      branch             = "ibcdpe-1007-monitoring"
+      branch             = var.branch
       description        = "Helm chart deployment for a single node Victoria Metrics instance"
       project_root       = "modules/victoria-metrics"
       space_id           = "root"
@@ -27,7 +79,7 @@ locals {
       name               = "trivy-operator"
       terraform_provider = "aws"
       administrative     = false
-      branch             = "ibcdpe-1007-monitoring"
+      branch             = var.branch
       description        = "Helm chart deployment for trivy-operator which handles security and vulnerability scanning."
       project_root       = "modules/trivy-operator"
       space_id           = "root"
@@ -44,109 +96,29 @@ locals {
       name               = "airflow"
       terraform_provider = "aws"
       administrative     = false
-      branch             = "ibcdpe-1007-monitoring"
+      branch             = var.branch
       description        = "Helm chart deployment for apache airflow."
       project_root       = "modules/apache-airflow"
       space_id           = "root"
       version_number     = "0.0.1"
     }
-  }
-}
 
-resource "spacelift_module" "sage-aws-vpc" {
-  github_enterprise {
-    namespace = "Sage-Bionetworks-Workflows"
-    id        = "sage-bionetworks-workflows-gh"
-  }
+    private-workerpool = {
+      github_enterprise = {
+        namespace = "Sage-Bionetworks-Workflows"
+        id        = "sage-bionetworks-workflows-gh"
+      }
+      repository = "eks-stack"
 
-  name               = "sage-aws-vpc"
-  terraform_provider = "aws"
-  administrative     = false
-  branch             = "ibcdpe-1007-monitoring"
-  description        = "Terraform module for creating a VPC in AWS"
-  repository         = "eks-stack"
-  project_root       = "modules/sage-aws-vpc"
-  space_id           = "root"
-}
-
-resource "spacelift_version" "sage-aws-vpc-version" {
-  module_id      = spacelift_module.sage-aws-vpc.id
-  version_number = "0.3.4"
-  keepers = {
-    "version" = "0.3.4"
-  }
-}
-
-resource "spacelift_module" "sage-aws-eks" {
-  github_enterprise {
-    namespace = "Sage-Bionetworks-Workflows"
-    id        = "sage-bionetworks-workflows-gh"
-  }
-
-  name               = "sage-aws-eks"
-  terraform_provider = "aws"
-  administrative     = false
-  branch             = "ibcdpe-1007-monitoring"
-  description        = "Terraform module for creating an EKS cluster in AWS"
-  repository         = "eks-stack"
-  project_root       = "modules/sage-aws-eks"
-  space_id           = "root"
-}
-
-resource "spacelift_version" "sage-aws-eks-version" {
-  module_id      = spacelift_module.sage-aws-eks.id
-  version_number = "0.3.10"
-  keepers = {
-    "version" = "0.3.10"
-  }
-}
-
-resource "spacelift_module" "sage-aws-eks-autoscaler" {
-  github_enterprise {
-    namespace = "Sage-Bionetworks-Workflows"
-    id        = "sage-bionetworks-workflows-gh"
-  }
-
-  name               = "sage-aws-eks-autoscaler"
-  terraform_provider = "aws"
-  administrative     = false
-  branch             = "ibcdpe-1007-monitoring"
-  description        = "Terraform module for creating an EKS cluster autoscaler in AWS"
-  repository         = "eks-stack"
-  project_root       = "modules/sage-aws-k8s-node-autoscaler"
-  space_id           = "root"
-}
-
-resource "spacelift_version" "sage-aws-eks-autoscaler-version" {
-  module_id      = spacelift_module.sage-aws-eks-autoscaler.id
-  version_number = "0.3.4"
-  keepers = {
-    "version" = "0.3.4"
-  }
-}
-
-resource "spacelift_module" "spacelift-private-workerpool" {
-  github_enterprise {
-    namespace = "Sage-Bionetworks-Workflows"
-    id        = "sage-bionetworks-workflows-gh"
-  }
-
-  name               = "spacelift-private-workerpool"
-  terraform_provider = "aws"
-  administrative     = false
-  branch             = "ibcdpe-1007-monitoring"
-  description        = "Module for the spacelift private workerpool helm chart which deploys the K8s operator"
-  repository         = "eks-stack"
-  project_root       = "modules/spacelift-private-worker"
-  space_id           = "root"
-
-}
-
-resource "spacelift_version" "spacelift-private-workerpool-version" {
-  module_id      = spacelift_module.spacelift-private-workerpool.id
-  version_number = "0.2.1"
-  keepers = {
-    "version" = "0.2.1"
+      name               = "spacelift-private-workerpool"
+      terraform_provider = "aws"
+      administrative     = false
+      branch             = var.branch
+      description        = "Module for the spacelift private workerpool helm chart which deploys the K8s operator"
+      project_root       = "modules/spacelift-private-worker"
+      space_id           = "root"
+      version_number     = "0.2.1"
+    }
   }
 }
 
