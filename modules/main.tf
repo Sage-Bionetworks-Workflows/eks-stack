c415a1e4fa8f19244ac80074044f7c2535a65e91
BryanFauble <17128019+BryanFauble@users.noreply.github.com>
2024-08-02 09:16:57 -0700

    Correct output

--- modules/main.tf
+++ modules/main.tf
@@ -32,7 +32,7 @@ locals {
       description        = "Terraform module for creating a VPC in AWS"
       project_root       = "modules/sage-aws-vpc"
       space_id           = "root"
-      version_number     = "0.4.1"
+      version_number     = "0.4.2"
     }
 
     eks-autoscaler = {
