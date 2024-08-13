5f65604bc12022c55fa3df5066685c6f550c21eb
BryanFauble <17128019+BryanFauble@users.noreply.github.com>
2024-08-13 11:54:12 -0700

    Increment

--- deployments/stacks/dpe-k8s-deployments/main.tf
+++ deployments/stacks/dpe-k8s-deployments/main.tf
@@ -12,7 +12,7 @@ module "sage-aws-eks-autoscaler" {
 
 module "victoria-metrics" {
   source  = "spacelift.io/sagebionetworks/victoria-metrics/aws"
-  version = "0.4.0"
+  version = "0.4.1"
 }
 
 module "trivy-operator" {
