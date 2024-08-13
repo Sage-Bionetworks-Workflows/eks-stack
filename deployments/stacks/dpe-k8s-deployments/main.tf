32b4e1e77b39b8f7b809b8321e3b89bed95c382d
BryanFauble <17128019+BryanFauble@users.noreply.github.com>
2024-08-13 12:29:51 -0700

    Bump VM

--- deployments/stacks/dpe-k8s-deployments/main.tf
+++ deployments/stacks/dpe-k8s-deployments/main.tf
@@ -13,7 +13,7 @@ module "sage-aws-eks-autoscaler" {
 module "victoria-metrics" {
   depends_on = [module.argo-cd]
   source     = "spacelift.io/sagebionetworks/victoria-metrics/aws"
-  version    = "0.4.2"
+  version    = "0.4.3"
 }
 
 module "trivy-operator" {
