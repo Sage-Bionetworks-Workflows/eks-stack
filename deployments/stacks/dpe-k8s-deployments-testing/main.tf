2eb76af5d36a6d375342a5ba1f64730c7b439195
BryanFauble <17128019+BryanFauble@users.noreply.github.com>
2024-08-16 11:23:13 -0700

    Bump version

--- deployments/stacks/dpe-k8s-deployments-testing/main.tf
+++ deployments/stacks/dpe-k8s-deployments-testing/main.tf
@@ -1,6 +1,6 @@
 module "postgres-cloud-native" {
   source       = "spacelift.io/sagebionetworks/postgres-cloud-native/aws"
-  version      = "0.2.0"
+  version      = "0.2.1"
   auto_deploy  = true
   auto_prune   = true
   git_revision = "ibcdpe-1004-airflow-ops"
