1c8f0145f8adcde28ac5b96a7accb2486f67096b
BryanFauble <17128019+BryanFauble@users.noreply.github.com>
2024-08-15 17:08:19 -0700

    Revert "Access my metadata"
    This reverts commit 405bb0c0d5d7cf6aac2d90c9c6dc3cf600a31004.

--- deployments/stacks/dpe-k8s-deployments/main.tf
+++ deployments/stacks/dpe-k8s-deployments/main.tf
@@ -57,7 +57,7 @@ resource "kubectl_manifest" "my-deployment" {
 apiVersion: apps/v1
 kind: Deployment
 metadata:
-  namespace: ${kubernetes_namespace.my-cool-resource.metadata.0.name}
+  namespace: ${kubernetes_namespace.my-cool-resource.name}
   name: flask-dep
   labels:
     app: flask-helloworld
