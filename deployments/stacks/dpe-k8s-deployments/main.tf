4a1e01a9504ba600c22a8f628cb2c68769fe116b
BryanFauble <17128019+BryanFauble@users.noreply.github.com>
2024-08-20 12:53:54 -0700

    Swap back to terraform registry

--- deployments/stacks/dpe-k8s-deployments/main.tf
+++ deployments/stacks/dpe-k8s-deployments/main.tf
@@ -10,9 +10,8 @@ module "sage-aws-eks-autoscaler" {
 }
 
 module "sage-aws-eks-addons" {
-  # source       = "spacelift.io/sagebionetworks/sage-aws-eks-addons/aws"
-  # version      = "0.2.0"
-  source             = "../../../modules/sage-aws-eks-addons"
+  source             = "spacelift.io/sagebionetworks/sage-aws-eks-addons/aws"
+  version            = "0.3.0"
   cluster_name       = var.cluster_name
   aws_account_id     = var.aws_account_id
   vpc_id             = var.vpc_id
