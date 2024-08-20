0a7ba61363e79243efd5f855e14dd24fef2f63a1
BryanFauble <17128019+BryanFauble@users.noreply.github.com>
2024-08-20 09:31:05 -0700

    Bump version

--- deployments/stacks/dpe-k8s-deployments/main.tf
+++ deployments/stacks/dpe-k8s-deployments/main.tf
@@ -1,7 +1,7 @@
 module "sage-aws-eks-autoscaler" {
   count   = 0
   source  = "spacelift.io/sagebionetworks/sage-aws-eks-autoscaler/aws"
-  version = "0.7.2"
+  version = "0.7.3"
 
   cluster_name           = var.cluster_name
   private_vpc_subnet_ids = var.private_subnet_ids
