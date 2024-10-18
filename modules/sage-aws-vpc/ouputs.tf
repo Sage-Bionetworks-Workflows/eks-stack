3011207ebe1d718193860860fddd56dfc40023d1
BryanFauble <17128019+BryanFauble@users.noreply.github.com>
2024-10-17 17:19:24 -0700

    Correct indexing

--- modules/sage-aws-vpc/ouputs.tf
+++ modules/sage-aws-vpc/ouputs.tf
@@ -27,7 +27,7 @@ output "vpc_private_subnet_cidrs" {
 }
 
 output "private_subnet_ids_eks_control_plane" {
-  value = slice(module.vpc.private_subnets, 0, length(var.private_subnet_cidrs_eks_control_plane) + 1)
+  value = slice(module.vpc.private_subnets, 0, length(var.private_subnet_cidrs_eks_control_plane))
 }
 
 output "private_subnet_ids_eks_worker_nodes" {
