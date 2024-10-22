c2a4bf8d7e2e443020b8cd70afa60518ef7826b4
BryanFauble <17128019+BryanFauble@users.noreply.github.com>
2024-10-22 09:36:11 -0700

    Correct vars

--- modules/cluster-ingress/variables.tf
+++ modules/cluster-ingress/variables.tf
@@ -30,3 +30,8 @@ variable "ssl_hostname" {
   description = "The hostname to use for the SSL certificate"
   type        = string
 }
+
+variable "cluster_issuer_name" {
+  description = "The name of the cluster issuer"
+  type        = string
+}
