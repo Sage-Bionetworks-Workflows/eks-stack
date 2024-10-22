7857c3b5a36f16fb6ec5d76bbebc2a3ec014478e
BryanFauble <17128019+BryanFauble@users.noreply.github.com>
2024-10-22 11:51:43 -0700

    import migration for dpe prod stack

--- deployments/stacks/dpe-k8s/main.tf
+++ deployments/stacks/dpe-k8s/main.tf
@@ -1,21 +1,21 @@
 import {
   to = module.sage-aws-vpc.module.vpc.aws_subnet.private[4]
-  id = "subnet-09b517a93fa33d7b2"
+  id = "subnet-07439a16803e8530a"
 }
 
 import {
   to = module.sage-aws-vpc.module.vpc.aws_subnet.private[2]
-  id = "subnet-0163cf7a8b19e5850"
+  id = "subnet-09b83ac65c65cfa04"
 }
 
 import {
   to = module.sage-aws-vpc.module.vpc.aws_route_table_association.private[4]
-  id = "subnet-09b517a93fa33d7b2/rtb-021bd3b745c8aaf1b"
+  id = "subnet-07439a16803e8530a/rtb-0bc7ba408812f5f83"
 }
 
 import {
   to = module.sage-aws-vpc.module.vpc.aws_route_table_association.private[2]
-  id = "subnet-0163cf7a8b19e5850/rtb-021bd3b745c8aaf1b"
+  id = "subnet-09b83ac65c65cfa04/rtb-0bc7ba408812f5f83"
 }
 
 module "sage-aws-vpc" {
