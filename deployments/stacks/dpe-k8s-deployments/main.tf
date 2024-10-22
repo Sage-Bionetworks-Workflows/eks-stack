e6fb41960640dd25a079741ee3d2c6e5754ac949
BryanFauble <17128019+BryanFauble@users.noreply.github.com>
2024-10-22 10:05:40 -0700

    Test deploying a temp merged gateway

--- deployments/stacks/dpe-k8s-deployments/main.tf
+++ deployments/stacks/dpe-k8s-deployments/main.tf
@@ -139,3 +139,20 @@ module "cert-manager" {
   namespace            = "cert-manager"
   argo_deployment_name = "cert-manager"
 }
+
+module "cluster-ingress-temp-testing-merged-gateway" {
+  depends_on = [module.argo-cd]
+  # source               = "spacelift.io/sagebionetworks/postgres-cloud-native-database/aws"
+  # version              = "0.5.0"
+  source               = "../../../modules/cluster-ingress"
+  auto_deploy          = var.auto_deploy
+  auto_prune           = var.auto_prune
+  git_revision         = var.git_revision
+  namespace            = "temp"
+  argo_deployment_name = "temp-cluster-ingress"
+
+  # To determine more elegant ways to fill in these values, for example, if we have
+  # a pre-defined DNS name for the cluster (https://sagebionetworks.jira.com/browse/IT-3931)
+  ssl_hostname        = "a32fb47f968e042fb938ac236202042a-1918252937.us-east-1.elb.amazonaws.com"
+  cluster_issuer_name = "selfsigned"
+}
