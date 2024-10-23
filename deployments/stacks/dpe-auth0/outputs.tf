7d037bddc39868bef92f7b51f8aec372092cc214
BryanFauble <17128019+BryanFauble@users.noreply.github.com>
2024-10-23 12:24:28 -0700

    Remove outputs

--- deployments/stacks/dpe-auth0/outputs.tf
+++
@@ -1,5 +0,0 @@
-output "auth0_clients" {
-  description = "All the Auth0 clients with name, ID, and secret"
-  value       = { for client in auth0_client.oauth2_clients : client.name => { id = client.id, secret = auth0_client_credentials.client_secrets[client.name].client_secret } }
-  sensitive   = true
-}
