7df3de095e57c37ac81e199d2a86681e029af575
BryanFauble <17128019+BryanFauble@users.noreply.github.com>
2024-10-23 12:22:24 -0700

    Output to sensitive

--- deployments/stacks/dpe-auth0/main.tf
+++ deployments/stacks/dpe-auth0/main.tf
@@ -27,6 +27,13 @@ resource "auth0_client" "oauth2_clients" {
   }
 }
 
+resource "auth0_client_credentials" "client_secrets" {
+  for_each = { for client in auth0_client.oauth2_clients : client.name => client }
+
+  client_id             = auth0_client.oauth2_clients[each.key].id
+  authentication_method = "client_secret_post"
+}
+
 resource "auth0_client_grant" "access_to_k8s_cluster" {
   for_each = { for client in var.auth0_clients : client.name => client }
 
