74ce0725852bea05c65726df200a3e89deef9e3a
BryanFauble <17128019+BryanFauble@users.noreply.github.com>
2024-10-25 11:51:17 -0700

    Move module import

--- modules/sage-aws-ses/main.tf
+++ modules/sage-aws-ses/main.tf
@@ -1,18 +1,3 @@
-import {
-  to = aws_ses_email_identity.identities[0]
-  id = "aws-dpe-dev@sagebase.org"
-}
-
-import {
-  to = aws_ses_email_identity.identities[1]
-  id = "bryan.fauble@sagebase.org",
-}
-
-import {
-  to = aws_ses_domain_identity.identities[0]
-  id = "sagebase.org",
-}
-
 resource "aws_ses_email_identity" "identities" {
   for_each = { for identity in var.email_identities : identity => identity }
   email = each.value
