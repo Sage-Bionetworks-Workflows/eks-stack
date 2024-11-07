ea309276a62244b4972a64c0cffbbddefdd1bae9
BryanFauble <17128019+BryanFauble@users.noreply.github.com>
2024-11-07 15:50:20 -0700

    TRY ANOTHER FILE FORMAT

--- modules/signoz-fluxcd/main.tf
+++ modules/signoz-fluxcd/main.tf
@@ -29,7 +29,7 @@ resource "kubernetes_config_map" "signoz-values" {
   }
 
   data = {
-    "signoz_values.yaml" = "${file("./templates/values.yaml")}"
+    "signoz_values.yaml" = "${file("${path.module}/templates/values.yaml")}"
   }
 
 }
