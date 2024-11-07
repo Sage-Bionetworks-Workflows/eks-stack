82e10cc39eb78f30a440fb28fb73868fee536649
BryanFauble <17128019+BryanFauble@users.noreply.github.com>
2024-11-07 14:28:40 -0700

    authenticate to github oci

--- deployments/stacks/dpe-k8s-deployments/provider.tf
+++ deployments/stacks/dpe-k8s-deployments/provider.tf
@@ -13,6 +13,14 @@ provider "helm" {
   kubernetes {
     config_path = var.kube_config_path
   }
+
+  registry {
+    url = "oci://ghcr.io"
+    # TODO: Is there a service account we can use instead of my personal account?
+    username = "BryanFauble"
+    # Requires that a secret be created in spacelift TF_VAR_github_container_repository_token
+    password = var.github_container_repository_token
+  }
 }
 
 provider "spotinst" {
