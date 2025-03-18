b524a5b501dc31118100041eafc028845c4ce3f9
BryanFauble <17128019+BryanFauble@users.noreply.github.com>
2025-03-18 09:49:50 -0700

    [DPE-1234] Create S3 croissant bucket in DPE aws account (#57)
    * Create S3 croissant bucket in dev aws account

--- modules/s3-bucket/main.tf
+++ modules/s3-bucket/main.tf
@@ -8,6 +8,52 @@ resource "aws_s3_bucket" "bucket" {
   )
 }
 
+resource "aws_s3_bucket_ownership_controls" "ownership" {
+  count = var.public_access ? 1 : 0
+  bucket = aws_s3_bucket.bucket.id
+  rule {
+    object_ownership = "BucketOwnerPreferred"
+  }
+}
+
+resource "aws_s3_bucket_public_access_block" "access_block" {
+  count = var.public_access ? 1 : 0
+  bucket = aws_s3_bucket.bucket.id
+
+  block_public_acls       = false
+  block_public_policy     = false
+  ignore_public_acls      = false
+  restrict_public_buckets = false
+}
+
+resource "aws_s3_bucket_acl" "bucket_acl" {
+  count = var.public_access ? 1 : 0
+  depends_on = [
+    aws_s3_bucket_ownership_controls.ownership,
+    aws_s3_bucket_public_access_block.access_block,
+  ]
+
+  bucket = aws_s3_bucket.bucket.id
+  acl    = "public-read"
+}
+
+resource "aws_s3_bucket_policy" "my_bucket_policy" {
+  count = var.public_access ? 1 : 0
+  bucket = aws_s3_bucket.bucket.bucket
+
+  policy = jsonencode({
+    Version = "2012-10-17",
+    Statement = [
+      {
+        Effect = "Allow",
+        Principal = "*",
+        Action = "s3:GetObject",
+        Resource = "${aws_s3_bucket.bucket.arn}/*",  # Allow access to all objects within the bucket
+      },
+    ],
+  })
+}
+
 resource "aws_s3_bucket_versioning" "versioning" {
   bucket = aws_s3_bucket.bucket.id
   versioning_configuration {
