14e0c67f687d3f0a330bc2e74fa758de8c3aef07
BryanFauble <17128019+BryanFauble@users.noreply.github.com>
2025-04-10 13:49:34 -0700

    [PORTALS-3539] Support enabling cors for S3 bucket (#60)
    * Support enabling cors for S3 bucket

--- modules/s3-bucket/main.tf
+++ modules/s3-bucket/main.tf
@@ -41,6 +41,8 @@ resource "aws_s3_bucket_policy" "my_bucket_policy" {
   count = var.public_access ? 1 : 0
   bucket = aws_s3_bucket.bucket.bucket
 
+  depends_on = [ aws_s3_bucket_acl.bucket_acl, aws_s3_bucket_public_access_block.access_block ]
+
   policy = jsonencode({
     Version = "2012-10-17",
     Statement = [
@@ -52,6 +54,16 @@ resource "aws_s3_bucket_policy" "my_bucket_policy" {
       },
     ],
   })
+}
+
+resource "aws_s3_bucket_cors_configuration" "cors" {
+  count = var.enable_cors ? 1 : 0
+  bucket = aws_s3_bucket.bucket.id
+
+  cors_rule {
+    allowed_methods = ["GET"]
+    allowed_origins = ["*"]
+  }
 }
 
 resource "aws_s3_bucket_versioning" "versioning" {
