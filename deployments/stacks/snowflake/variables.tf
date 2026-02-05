451e023e24aa0fdc82a5c53fb6f922a2a76c4c86
Jenny Medina <medina.stsci@gmail.com>
2026-02-05 09:30:05 -0500

    snowflake stack main and variables tf updated accordingly

--- deployments/stacks/snowflake/variables.tf
+++ deployments/stacks/snowflake/variables.tf
@@ -2,3 +2,19 @@ variable "aws_account_id" {
   description = "AWS account ID where S3 resources will be created"
   type        = string
 }
+
+variable "region" {
+  description = "AWS region for S3 bucket"
+  type        = string
+  default     = "us-east-1"
+}
+
+variable "source_account_id" {
+  description = "AWS account ID that will replicate TO this bucket (for cross-account replication)"
+  type        = string
+}
+
+variable "source_bucket_arn" {
+  description = "ARN of the source bucket that will replicate to this bucket"
+  type        = string
+}
