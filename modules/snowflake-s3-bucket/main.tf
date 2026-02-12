00fecaabcec0d1a2fd812b6b53ba8dbc586de165
Jenny Medina <medina.stsci@gmail.com>
2026-02-12 14:28:50 -0500

    create kms key with appropriate permissions

--- modules/snowflake-s3-bucket/main.tf
+++ modules/snowflake-s3-bucket/main.tf
@@ -62,7 +62,8 @@ resource "aws_s3_bucket_policy" "replication_destination_policy" {
         Action = [
           "s3:ReplicateObject",
           "s3:ReplicateDelete",
-          "s3:ReplicateTags"
+          "s3:ReplicateTags",
+          "s3:ObjectOwnerOverrideToBucketOwner"
         ]
         Resource = "${aws_s3_bucket.bucket.arn}/*"
       },
@@ -77,18 +78,109 @@ resource "aws_s3_bucket_policy" "replication_destination_policy" {
           "s3:GetBucketVersioning"
         ]
         Resource = aws_s3_bucket.bucket.arn
+      },
+      {
+        Sid    = "AllowCrossAccountKMSAccess"
+        Effect = "Allow"
+        Principal = {
+          AWS = "arn:aws:iam::${var.source_account_id}:root"
+        }
+        Action = [
+          "s3:PutObject",
+          "s3:PutObjectAcl"
+        ]
+        Resource = "${aws_s3_bucket.bucket.arn}/*"
+        Condition = {
+          StringEquals = {
+            "s3:x-amz-server-side-encryption" = "aws:kms"
+            "s3:x-amz-server-side-encryption-aws-kms-key-id" = aws_kms_key.dpe_encryption_key.arn
+          }
+        }
       }
     ]
   })
 }
 
-# Enable server-side encryption by default
+# Customer-managed KMS key for S3 encryption
+resource "aws_kms_key" "dpe_encryption_key" {
+  description = "KMS key for S3 encryption in ${var.bucket_name}"
+
+  policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [
+      # Statement 1: Destination (DPE) Account Admin permissions
+      {
+        Sid    = "AllowAdminInDestinationAccount"
+        Effect = "Allow"
+        Principal = {
+          AWS = "arn:aws:iam::${var.aws_account_id}:root"
+        }
+        Action   = "kms:*"
+        Resource = "*"
+      },
+
+      # Statement 2: Source (Platform) Account permissions
+      {
+        Sid    = "AllowCrossAccountReplicationRoleAccess"
+        Effect = "Allow"
+        # TODO: Tighten this up to just grant permissions to the
+        #       source account's replication role
+        Principal = {
+          AWS = "arn:aws:iam::${var.source_account_id}:root"
+        }
+        Action = [
+          "kms:Encrypt",
+          "kms:Decrypt",
+          "kms:ReEncrypt*",
+          "kms:GenerateDataKey",
+          "kms:DescribeKey"
+        ]
+        Resource = "*"
+      },
+
+      # Statement 3: S3 Service cryptographic permissions
+      {
+        Sid    = "AllowS3ServiceAccess"
+        Effect = "Allow"
+        Principal = {
+          Service = "s3.amazonaws.com"
+        }
+        Action = [
+          "kms:Decrypt",
+          "kms:GenerateDataKey"
+        ]
+        Resource = "*"
+        Condition = {
+          StringEquals = {
+            "kms:ViaService" = "s3.${var.region}.amazonaws.com"
+          }
+        }
+      }
+    ]
+  })
+
+  tags = merge(
+    var.tags,
+    {
+      Name    = "dpe_encryption_key"
+      Purpose = "S3 encryption for DPE Snowflake bucket $(var.bucket_name)"
+    }
+  )
+}
+
+resource "aws_kms_alias" "dpe_encryption_key_alias" {
+  name          = "alias/${var.bucket_name}-encryption"
+  target_key_id = aws_kms_key.dpe_encryption_key.key_id
+}
+
+# Set up encryption using customer-managed KMS
 resource "aws_s3_bucket_server_side_encryption_configuration" "encryption" {
   bucket = aws_s3_bucket.bucket.id
 
   rule {
     apply_server_side_encryption_by_default {
-      sse_algorithm = "AES256"
+      sse_algorithm     = "aws:kms"
+      kms_master_key_id = aws_kms_key.dpe_encryption_key.arn
     }
     bucket_key_enabled = true
   }
--- modules/snowflake-s3-bucket/outputs.tf
+++ modules/snowflake-s3-bucket/outputs.tf
@@ -22,3 +22,18 @@ output "bucket_regional_domain_name" {
   description = "Bucket regional domain name"
   value       = aws_s3_bucket.bucket.bucket_regional_domain_name
 }
+
+output "kms_key_id" {
+  description = "ID of the KMS key used for S3 encryption"
+  value       = aws_kms_key.dpe_encryption_key.key_id
+}
+
+output "kms_key_arn" {
+  description = "ARN of the KMS key used for S3 encryption"
+  value       = aws_kms_key.dpe_encryption_key.arn
+}
+
+output "kms_key_alias" {
+  description = "Alias of the KMS key used for S3 encryption"
+  value       = aws_kms_alias.dpe_encryption_key_alias.name
+}
